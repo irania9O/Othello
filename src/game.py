f679a9723945de2d6c6771eb6d4cc8e19f2ff528
irania90 <wwwwwq37@gmail.com>
2022-08-31 16:14:05 +0430

    add pieces count calculate and show sount.

--- src/game.py
+++ src/game.py
@@ -1,8 +1,9 @@
 from src.board import Board
 from src.constant import *
-import pygame
 from src.piece import Piece
 from src.square import Square
+import pygame
+import os
 
 class Game:
 
@@ -12,19 +13,43 @@ class Game:
         self.board = Board()
         self.possible_squares = []
         self.reverse_squares = []
+        self.black_pieces = 0
+        self.white_pieces = 0
 
     def show_board(self):
+        self.screen.fill((0, 144, 103))
         for row in range(ROWS):
             for col in range(COLS):
-                color = (0, 144, 103)
-                rect = (col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
-                pygame.draw.rect(self.screen, color, rect)
-
+                # color = (0, 144, 103)
+                # rect = (col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
+                # pygame.draw.rect(self.screen, color, rect)
                 color = (0, 0, 0)
                 rect = (col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
-                pygame.draw.rect(self.screen, color, rect, width =1)
+                pygame.draw.rect(self.screen, color, rect, width = 1)
+
+                self.calculate_pieces()
+                self.font = pygame.font.SysFont('arial', 22)
+
+                img_black = pygame.image.load(os.path.join(f"assets/images/black-100.png"))
+                img_black_center = 50, 650
+                self.screen.blit(img_black, img_black.get_rect(center=img_black_center))
+                self.label = self.font.render(f"{self.black_pieces}", 1, (255,255,255))
+                label_position = [40 , 637]
+                if self.black_pieces < 10:
+                    label_position[0] = 45
+                self.screen.blit(self.label, label_position)
+
+                img_white = pygame.image.load(os.path.join(f"assets/images/white-100.png"))
+                img_white_center = 550, 650
+                self.screen.blit(img_white, img_white.get_rect(center=img_white_center))
+                self.label = self.font.render(f"{self.white_pieces}", 1, (0,0,0))
+                label_position = [540 , 637]
+                if self.white_pieces < 10:
+                    label_position[0] = 545
+                self.screen.blit(self.label, label_position)                
 
     def show_pieces(self):
+
         for row in range(ROWS):
             for col in range(COLS):
                 # if we have pice on specific squares
@@ -139,3 +164,15 @@ class Game:
 
     def next_turn(self):
         self.turn = "white" if self.turn == "black" else "black"
+
+    def calculate_pieces(self):
+        self.black_pieces = 0
+        self.white_pieces = 0
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.board.squares[row][col].has_piece():
+                    if self.board.squares[row][col].piece.color == "black":
+                        self.black_pieces += 1
+                    elif self.board.squares[row][col].piece.color == "white":
+                        self.white_pieces += 1
+ 
